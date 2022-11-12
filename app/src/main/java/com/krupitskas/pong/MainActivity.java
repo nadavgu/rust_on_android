09f521b1e2f367ab4874dc67d55727fd4723949a
IronForehead <nadavgu@gmail.com>
2022-11-12 15:31:20 +0200

    Removed fractals

--- app/src/main/java/com/krupitskas/pong/MainActivity.java
+++ app/src/main/java/com/krupitskas/pong/MainActivity.java
@@ -15,10 +15,5 @@ public class MainActivity extends AppCompatActivity {
 
         String r = RustBindings.greeting("Android world");
         ((TextView)findViewById(R.id.helloWorldText)).setText(r);
-
-        Bitmap bitmap = Bitmap.createBitmap(800, 800, Bitmap.Config.ARGB_8888);
-        RustBindings.renderFractal(bitmap);
-        ImageView imageView = findViewById(R.id.imageView);
-        imageView.setImageBitmap(bitmap);
     }
 }
