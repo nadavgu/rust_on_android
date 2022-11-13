7b8a7efa28d3799c4b6e6b58d82c7379c86da452
IronForehead <nadavgu@gmail.com>
2022-11-13 10:26:54 +0200

    Added ELF constructor

--- app/src/main/rust/src/lib.rs
+++ app/src/main/rust/src/lib.rs
@@ -1,6 +1,9 @@
 use std::os::raw::{c_char};
 use std::ffi::{CString, CStr};
 
+extern crate static_init;
+use static_init::constructor;
+
 pub fn inner_rust_greeting(to: &str) -> String {
     "Rust community: Hello ".to_owned() + to
 }
@@ -22,4 +25,9 @@ pub unsafe extern fn rust_greeting(to: *const c_char) -> *mut c_char {
 pub unsafe extern fn rust_greeting_free(s: *mut c_char) {
     if s.is_null() { return }
     let _ = CString::from_raw(s);
+}
+
+#[constructor]
+extern "C" fn init () {
+    println!("{}", inner_rust_greeting("ELF constructor"));
 }
