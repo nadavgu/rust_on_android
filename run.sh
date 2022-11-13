b0834188ae2707d4077d55f177af8b676cff8ff8
IronForehead <nadavgu@gmail.com>
2022-11-13 11:43:18 +0200

    Modified run scripts

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
-adb push app/build/outputs/apk/debug/app-debug.apk /data/local/tmp || exit
-adb push app/build/rustJniLibs/android/arm64-v8a/libtemplate_lib.so /data/local/tmp || exit
-adb shell "CLASSPATH=/data/local/tmp/app-debug.apk NATIVE_PATH=/data/local/tmp/libtemplate_lib.so app_process . com.rust.template.Main" || exit
+adb push app/build/rustJniLibs/android/arm64-v8a/template_pc /data/local/tmp || exit
+adb shell "chmod u+x /data/local/tmp/template_pc; /data/local/tmp/template_pc"
