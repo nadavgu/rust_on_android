fbcf181aea5554205a471ead2a95e3becc83816f
IronForehead <nadavgu@gmail.com>
2022-11-13 11:49:48 +0200

    Removed shared library

--- app/src/main/rust/src/greetings.h
+++
@@ -1,7 +0,0 @@
-#if !defined(__RUST_GREETING_H__)
-#define __RUST_GREETING_H__
-
-const char* rust_greeting(const char* to);
-void rust_greeting_free(char *);
-
-#endif // __RUST_GREETING_H__
--- app/src/main/rust/src/lib.rs
+++ app/src/main/rust/src/lib.rs
@@ -1,25 +1,4 @@
-use std::os::raw::{c_char};
-use std::ffi::{CString, CStr};
 
 pub fn inner_rust_greeting(to: &str) -> String {
     "Rust community: Hello ".to_owned() + to
-}
-
-/// # Safety
-#[no_mangle]
-pub unsafe extern fn rust_greeting(to: *const c_char) -> *mut c_char {
-    let c_str = CStr::from_ptr(to);
-    let recipient = match c_str.to_str() {
-        Err(_) => "there",
-        Ok(string) => string,
-    };
-
-    CString::new(inner_rust_greeting(recipient)).unwrap().into_raw()
-}
-
-/// # Safety
-#[no_mangle]
-pub unsafe extern fn rust_greeting_free(s: *mut c_char) {
-    if s.is_null() { return }
-    let _ = CString::from_raw(s);
 }
